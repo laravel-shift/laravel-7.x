008c21eca7ce7d2b34aa378413ee1ee680da1156
Laravel Shift <shift@laravelshift.com>
2020-12-14 22:43:05 +0000

    Shift to class based factories

--- app/User.php
+++ app/User.php
@@ -2,12 +2,15 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
+    use HasFactory;
+
     use Notifiable;
 
     /**
